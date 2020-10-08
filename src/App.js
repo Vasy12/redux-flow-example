d6102bec6a08fd3b797e97945d440cfc6cc6ce30
User Name <artur.it2914@gmail.com>
2020-10-08 18:43:47 +0300

    Add redux flow example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return <></>;
+function App(props) {
+  const { value, step, increment, decrement, onStepChange } = props;
+
+  return (
+    <article>
+      <h1>{value}</h1>
+      <div>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+      </div>
+      <hr />
+      <input type="number" value={step} onChange={onStepChange} />
+    </article>
+  );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return state;
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    increment: () =>
+      dispatch({
+        type: 'INCREMENT',
+      }),
+    decrement: () =>
+      dispatch({
+        type: 'DECREMENT',
+      }),
+    onStepChange: ({ target: { value } }) =>
+      dispatch({
+        type: 'SET_STEP',
+        stepValue: Number(value),
+      }),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = {
+  value: 0,
+  step: 1,
+};
+
+function reducer(state = initialState, action) {
+  const { type } = action;
+  switch (type) {
+    case 'INCREMENT': {
+      const { value, step } = state;
+      return {
+        ...state,
+        value: value + step,
+      };
+    }
+    case 'DECREMENT': {
+      const { value, step } = state;
+      return {
+        ...state,
+        value: value - step,
+      };
+    }
+    case 'SET_STEP': {
+      const { stepValue } = action;
+      return {
+        ...state,
+        step: stepValue,
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
